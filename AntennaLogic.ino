1cf92bc97f83184483cae68ac5aeaa7d881080b7
LanceWoodyClifford <cliffordlw@gmail.com>
2022-05-02 15:36:35 -0700

    New code for switching antennas plus integrating twinkle light switch into one box, thus freeing up a relay and nano processor.

--- AntennaLogic.ino
+++ AntennaLogic.ino
@@ -23,28 +23,31 @@ int rotateDirClockwise = false;
 int rotateStep = 5;
 int button = 0;
 int currentButton = 0;
+bool first = false;
+
 int sameButtonLoopCounter = 0;
 volatile int state = LOW;  // relay off left connector
  volatile unsigned long milliSecondsAtInterrupt = 0;
  unsigned long milliSecondsNow = 0;
- unsigned long runIntervalInMilliseconds = 1000;
+ unsigned long runIntervalInMilliseconds = 10000;
  
 void setup() {
   
   Serial.begin(9600);
-  // irrecv.enableIRIn(); // Start the receiver
+
   IrReceiver.begin(RECV_PIN);
   Serial.println("Start");
   pinMode (servoRotatePin, OUTPUT);
   
   servoRotate.attach(servoRotatePin);
     servoRotate.write(45);
-    delay(100);
-    pinMode(LED_BUILTIN,OUTPUT);
+    currentPosRotate = 45;
+    delay(200);
+  //  pinMode(LED_BUILTIN,OUTPUT);
     pinMode(antennaLeftRightPin, OUTPUT);
-    digitalWrite(LED_BUILTIN,LOW);
+    pinMode(twinkleLightsOnOff, OUTPUT);
+  //  digitalWrite(LED_BUILTIN,LOW);
     digitalWrite(antennaLeftRightPin, LOW);
- //   sleepNow();
 }
 
 void loop() {
@@ -53,17 +56,16 @@ void loop() {
    { 
      IrReceiver.printIRResultAsCVariables(&Serial);
      command = IrReceiver.decodedIRData.command;
-     Serial.println(command);
-    delay(100);
+     
+     if(command != 0) Serial.println(command);
+     delay(100);
        if ( command == 25 )  
        {    
          currentButton = rotateClockwise;
-         digitalWrite(antennaLeftRightPin, HIGH);
        }
        if (command == 29 )
        {
         currentButton = rotateCounterClockwise;
-        digitalWrite(antennaLeftRightPin, LOW);
        } 
        // channel 2, 6 & 8 position
        if ( command == 130 )
@@ -106,10 +108,7 @@ void loop() {
        }
        
     IrReceiver.resume();        // prepare to receive the next value
-    if(currentButton == 0){
-      delay(200);
-//      sleepNow();
-    }
+    delay(200);
    }  
 
   if(currentButton > 0) {
@@ -128,8 +127,8 @@ void loop() {
         Serial.print("Rotate to ");
         Serial.println(currentPosRotate);
         servoRotate.write(currentPosRotate);
-        delay(300);
-//        sleepNow();
+        delay(500);
+    //    sleepNow();
         break;
 
       case rotateCounterClockwise:
@@ -141,55 +140,61 @@ void loop() {
         Serial.print("Rotate to ");
         Serial.println(currentPosRotate);
         servoRotate.write(currentPosRotate);
-        delay(300);
-//        sleepNow();
+        delay(500);
+     //   sleepNow();
         break;
 
-      //default: sleepNow();
+//      default: sleepNow();
     }
   }
 }
 
+
+/* Not working 
 void sleepNow()
 {
 //print message to serial monitor to let the user know board has gone to sleep
+
 Serial.println("going to sleep");
+
 //delay is added to allow user to get the full message on the serial monitor before going to sleep
-delay(200);
+delay(2000);
+attachInterrupt(digitalPinToInterrupt(RECV_PIN), wakeupNow, RISING);
 
 // This is where we enable the interrupt, the reason it is done here is so that if the button is pressed accidently it doesn’t interrupt the running program.  
-attachInterrupt(interruptPin3, wakeupNow, RISING);
+
 
 //enables the sleep mode
 sleep_enable();
 
-/* The next line is where we choose the sleep mode we want to use for this code.  
- *  There are a few options to choose from, each with their own uses.  
- *  For more information on the sleep modes, please review the Atmega8 datasheet at [http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega48A-PA-88A-PA-168A-PA-328-P-DS-DS40002061A.pdf](http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega48A-PA-88A-PA-168A-PA-328-P-DS-DS40002061A.pdf)
+// The next line is where we choose the sleep mode we want to use for this code.  
+//  There are a few options to choose from, each with their own uses.  
+//  For more information on the sleep modes, please review the Atmega8 datasheet at [http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega48A-PA-88A-PA-168A-PA-328-P-DS-DS40002061A.pdf](http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega48A-PA-88A-PA-168A-PA-328-P-DS-DS40002061A.pdf)
 
-The 5 different options for sleep modes, they are listed below from least power savings to most power savings:
-  SLEEP_MODE_IDLE
-  SLEEP_MODE_ADC
-  SLEEP_MODE_PWR_SAVE
-  SLEEP_MODE_STANDBY
-  SLEEP_MODE_PWR_DOWN
-
-For this sketch, we will be using the most power savings possible so we choose SLEEP_MODE_PWR_DOWN */
+//The 5 different options for sleep modes, they are listed below from least power savings to most power savings:
+//  SLEEP_MODE_IDLE
+//  SLEEP_MODE_ADC
+//  SLEEP_MODE_PWR_SAVE
+//  SLEEP_MODE_STANDBY
+ // SLEEP_MODE_PWR_DOWN
+//For this sketch, we will be using the most power savings possible so we choose SLEEP_MODE_PWR_DOWN 
 
 //sleep mode is set here
-set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+set_sleep_mode(SLEEP_MODE_STANDBY);
 
 
 //This is where the device is actually put to sleep
 sleep_mode();
-detachInterrupt(interruptPin3);
 }
 
 void wakeupNow(){
-  sleep_disable();
-  // irrecv.resume();
+    sleep_disable();
+    detachInterrupt(digitalPinToInterrupt(RECV_PIN));
+    
+    Serial.println("waking up");
+    delay(300);
 }
 
- 
+ */
 
        
